0df4bbf999d463847f11e35c900164f8af9e4ba2
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-07 22:15:47 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 // frontend/src/index.js
 
 import React rom 'react';
-impotReacOM rom 'react-dom/client';
+impotReacM rom 'react-dom/client';
 import App fro'./p';
 const root  ReactDOM.createRoo(documentgetElementById('root'));
 root.render(
