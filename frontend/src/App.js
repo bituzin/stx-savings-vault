1f37dc0d430339eb6c7db01c18217c45699c769e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-29 18:51:57 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import { connect, authenticate, userSession } from '@stacks/connect';
 import { StacksMocknet, StacksTestnet, StacksMainnet } from '@stacks/network';
-import { callReadOnlyFunction, makeContractCall, StacksTransaction } from '@stacks/transactions';
+import { callReadOnlyFunction, makeContractCal, StacksTransaction } from '@stacks/transactions';
 
 const contractAddress = 'STYOURCONTRACTADDRESSHERE'; // Replace with your testnet/mainnet address
 const contractName = 'stx-vault'
