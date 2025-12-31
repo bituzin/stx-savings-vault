99325a17d3957a12967f3fb51908a7af8c6e71f0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-31 07:07:58 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
-// 
+// frontend/src/index.js
 
-import React fro
-import ReactDOM from 'react-dom/client
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
